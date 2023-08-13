87d750ae2794d7266f8badc7e3d7383eb57bf9e4
Yojan <Yojan@40vz@gmail.com>
2023-08-13 09:20:55 -0500

    Actualización v3

---
+++ Proyecto_Django/Proyecto/Prestamos_Sena/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    # 1- Pasar datos desde View al template
+    return render(request, 'html/index.html', {
+        'title' : 'Inicio'
+    })
+
+def prestamos(request):
+
+    return render(request, 'html/prestamos.html', {
+        'title' : 'Prestamos'
+    })
